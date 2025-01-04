2d22e7deaa075364ffd001f3766ec061f3b08412
georgewilli <george.william@titrias.com>
2025-01-05 01:48:17 +0200

    ...

--- src/order-item/order-item.service.ts
+++ src/order-item/order-item.service.ts
@@ -1,21 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import { PushoverService } from 'src/pushover/pushover.service';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateOrderItemDTO } from './dto/create-order-item-dto';
 import { OrderItemRepository } from './order-item.repository';
 import { OrderItemDTO } from './dto/order-item.dto';
 
+
 @Injectable()
 export class OrderItemService {
-  constructor(private orderItemRepository: OrderItemRepository) {}
+  
+  constructor(  
+    private orderItemRepository: OrderItemRepository,
+    ){
+  }
   async create(orderData: CreateOrderItemDTO) {
-    const newOrder = await this.orderItemRepository.create(orderData);
+    const newOrder = await this.orderItemRepository.create(
+       orderData,
+    );
     return newOrder;
   }
+
   async createOrderItems(orderId: number, items: OrderItemDTO[]): Promise<void> {
-    const createPromises = items.map((item) =>
-      this.create({ orderId, productId: item.productId, quantity: item.quantity }),
+    const createPromises = items.map(item =>
+      this.create({orderId, productId : item.productId , quantity: item.quantity}),
     );
-    await Promise.all(createPromises); // Execute all creations concurrently
+    await Promise.all(createPromises); 
   }
 }
